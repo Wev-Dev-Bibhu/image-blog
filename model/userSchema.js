8d2f2de8d12b1a06be48713b6e1f7ccab0d3c56a
Wev-Dev-Bibhu <iambibhutidas@gmail.com>
2023-01-14 17:31:24 +0530

    updating schema

--- model/userSchema.js
+++ model/userSchema.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
     username: {
