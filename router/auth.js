1a56e86344228a0120854a79de20883f3ca65cef
Wev-Dev-Bibhu <iambibhutidas@gmail.com>
2023-01-18 00:27:35 +0530

    updaing cors err

--- router/auth.js
+++ router/auth.js
@@ -8,6 +8,7 @@ const authenticatePage = require('../middleware/authenticate')
 
 router.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://image-blog-site.netlify.app") // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://image-blog-site.netlify.app/*") // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', true);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
